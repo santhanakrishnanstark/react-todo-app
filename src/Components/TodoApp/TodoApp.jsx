12ea93d1c42713e9a2401e3943b534d6cb44f68b
santhanakrishnanstark <santhanakrishnanstark@gmail.com>
2021-11-29 22:02:28 +0530

    Todo app initial commit with base setup

---
+++ src/Components/TodoApp/TodoApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoApp = () => {
+    return ( 
+        <>
+            <h1>TodoApp</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam a quo doloribus delectus minima distinctio eum sed omnis architecto! Quasi pariatur vero omnis vitae odio possimus beatae, voluptatem laudantium deserunt.</p>
+        </>
+     );
+}
+ 
+export default TodoApp;
